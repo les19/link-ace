176ff901b95855d94330392a2af174f7c74d1cb3
Oleksii Muntian <lex.19.mov@gmail.com>
2024-04-17 12:47:12 +0300

    Updated footer.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,4 @@
 <aside class="footer container text-center small pt-3 pb-5">
-    <div>
-        @lang('linkace.project_of') <a href="https://kovah.de/?utm_source=linkace" rel="noopener" target="_blank">Kovah.de</a>
-    </div>
     @auth
         <div class="mt-1">
             @lang('linkace.version', ['version' => \App\Helper\UpdateHelper::currentVersion()]) -
